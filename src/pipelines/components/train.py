afb751cd227d87a44fb1a122f0fbb9cf3bbe84c2
kubeflow notebook <user@example.com>
2026-01-31 23:45:38 +0000

    training step, with small dsl.Parallel loop to test control flow structures

--- src/pipelines/components/train.py
+++ src/pipelines/components/train.py
@@ -2,19 +2,138 @@ from kfp import dsl
 
 
 @dsl.component(
-    base_image="pytorch/pytorch:2.9.1-cuda12.6-cudnn9-runtime",
-    packages_to_install=["pandas"],
+    base_image="pytorch/pytorch:2.8.0-cuda12.9-cudnn9-runtime",
+    packages_to_install=["pandas==2.3.3"],
 )
 def train(
     train_dataset: dsl.Input[dsl.Dataset],
     test_dataset: dsl.Input[dsl.Dataset],
-    model: dsl.Output[dsl.Model],
+    kfp_model: dsl.Output[dsl.Model],
+    learning_rate: float = 1e-3,
+    n_epochs: int = 1,
 ):
-    def build_model():
-        pass
+    from typing import Tuple
+    import pandas as pd
+    import torch
 
-    def build_data_loader():
-        pass
+    class PandasDataset(torch.utils.data.Dataset):
+        def __init__(self, csv_path: str, target_col: str):
+            dataset = pd.read_csv(csv_path)
+            self.features = dataset.drop(target_col, axis=1)
+            self.labels = dataset[target_col]
 
-    def train_model():
-        pass
+        def __len__(self):
+            return self.features.shape[0]
+
+        def __getitem__(self, idx: int):
+            return (
+                torch.tensor(self.features.iloc[idx], dtype=torch.float32),
+                torch.tensor(self.labels.iloc[idx], dtype=torch.long),
+            )
+
+    class NNClassifier(torch.nn.Module):
+        def __init__(self, layer_config: Tuple[Tuple[int, int]]):
+            super().__init__()
+            self.layers = torch.nn.Sequential()
+            for i, (layer_in, layer_out) in enumerate(layer_config):
+                self.layers.append(torch.nn.Linear(layer_in, layer_out))
+                if i < len(layer_config) - 1:
+                    self.layers.append(torch.nn.ReLU())
+
+        def forward(self, x):
+            return self.layers(x)
+
+    def train_model(
+        model: NNClassifier,
+        dataloader: torch.utils.data.DataLoader,
+        optimizer: torch.optim.Optimizer,
+        loss_fn: torch.nn.Module,
+        device: str,
+    ):
+        model.train()
+
+        train_size = len(dataloader.dataset)
+        for batch, (x_train, y_train) in enumerate(dataloader):
+            x_train = x_train.to(device)
+            y_train = y_train.to(device)
+
+            y_pred = model(x_train)
+            loss = loss_fn(y_pred, y_train)
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if batch % 10 == 0:
+                loss, current = loss.item(), (batch + 1) * len(x_train)
+                print(f"loss: {loss:>7f}  [{current:>5d}/{train_size:>5d}]")
+
+    def evaluate_model(
+        model: NNClassifier,
+        dataloader: torch.utils.data.DataLoader,
+        loss_fn: torch.nn.Module,
+        device: str,
+    ):
+        model.eval()
+        test_loss, n_correct = 0, 0
+        with torch.no_grad():
+            for x_test, y_test in dataloader:
+                x_test = x_test.to(device)
+                y_test = y_test.to(device)
+                y_pred = model(x_test)
+                test_loss += loss_fn(y_pred, y_test).item()
+                n_correct += (y_pred.argmax(1) == y_test).type(torch.float).sum().item()
+
+        print(
+            f"Accuracy: {(100 * n_correct / len(dataloader.dataset)):>0.1f}%, Avg. loss: {test_loss / len(dataloader):>8f} \n"
+        )
+
+    def train_loop(
+        model: NNClassifier,
+        train_dataloader: torch.utils.data.DataLoader,
+        test_dataloader: torch.utils.data.DataLoader,
+        loss_fn: torch.nn.Module,
+        optimizer: torch.optim.Optimizer,
+        n_epochs: int,
+        device: str,
+    ):
+        for i in range(n_epochs):
+            print(f"Epoch {i + 1}\n------------------------------------------")
+            train_model(
+                model=model,
+                dataloader=train_dataloader,
+                loss_fn=loss_fn,
+                optimizer=optimizer,
+                device=device,
+            )
+            evaluate_model(
+                model=model, dataloader=test_dataloader, loss_fn=loss_fn, device=device
+            )
+        print("Training finished.")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    train_dataset = PandasDataset(csv_path=train_dataset.path, target_col="RiskLevel")
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=32, shuffle=True
+    )
+    test_dataset = PandasDataset(csv_path=test_dataset.path, target_col="RiskLevel")
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=32, shuffle=True
+    )
+
+    model = NNClassifier(layer_config=((6, 6), (6, 3))).to(device)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    train_loop(
+        model=model,
+        train_dataloader=train_dataloader,
+        test_dataloader=test_dataloader,
+        loss_fn=loss_fn,
+        optimizer=optimizer,
+        n_epochs=n_epochs,
+        device=device,
+    )
+
+    torch.save(model.state_dict(), kfp_model.path)
--- src/pipelines/end-to-end.py
+++ src/pipelines/end-to-end.py
@@ -2,15 +2,25 @@ from kfp import dsl, client
 from pipelines.components import extract_data as extractor
 from pipelines.components import data_visualization as visualization
 from pipelines.components import feature_transformation as feature_transformator
+from pipelines.components import train as trainer
 
 
 @dsl.pipeline(name="maternity-prediction")
 def maternity_prediction_pipeline():
     extract_task = extractor.extract_data()
     visualization.visualize_data(dataset=extract_task.output)
-    transformation_task = feature_transformator.feature_transformation(raw_dataset=extract_task.output, test_size=0.2)
+    transformation_task = feature_transformator.feature_transformation(
+        raw_dataset=extract_task.output, test_size=0.2
+    )
     visualization.visualize_data(dataset=transformation_task.outputs["train_dataset"])
     visualization.visualize_data(dataset=transformation_task.outputs["test_dataset"])
+    with dsl.ParallelFor([1e-2, 1e-3, 1e-4]) as learning_rate:
+        trainer.train(
+            train_dataset=transformation_task.outputs["train_dataset"],
+            test_dataset=transformation_task.outputs["test_dataset"],
+            learning_rate=learning_rate,
+            n_epochs=3,
+        )
 
 
 if __name__ == "__main__":
